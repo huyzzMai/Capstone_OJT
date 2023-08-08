e4bc268746d4b8f693cba6c1040af3d58a318e26
huyzzMai <giahuy120701@gmail.com>
2023-08-08 21:37:33 +0700

    Add migration

---
+++ DataAccessLayer/Migrations/20230808143653_AdjustUserTbl.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class AdjustUserTbl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "User",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "User",
+                type: "nvarchar(100)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentCode",
+                table: "User",
+                type: "nvarchar(20)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "StudentCode",
+                table: "User");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "User",
+                newName: "Name");
+        }
+    }
+}
