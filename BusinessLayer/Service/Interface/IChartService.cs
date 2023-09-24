bd505f591dc195cac3b4e923b203222718273a3c
tuan <tuanpkasd124@gmail.com>
2023-09-24 13:12:30 +0700

    add new  api

--- BusinessLayer/Service/Interface/IChartService.cs
+++ BusinessLayer/Service/Interface/IChartService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Payload.ResponseModel.ChartResponse;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace BusinessLayer.Service.Interface
 {
     public interface IChartService
     {
+        Task<BatchAndTraineeResponse> getBatchAndTrainee(int year);
+
+        Task<List<ChartCircleResponse>> getTrainerWithMostTrainees(int number);
+
+        Task<List<ChartCircleResponse>> getTraineeByPosition();
     }
 }
